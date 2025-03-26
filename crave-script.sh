5ec6e6aed67f58bb3e098551c47115f7c403f395
Angelpro09xd <167892325+Angelpro09xd@users.noreply.github.com>
2025-03-26 19:48:07 +0100

    Update crave-script.sh rm megvii

--- crave-script.sh
+++ crave-script.sh
@@ -63,4 +63,5 @@ export BUILD_USERNAME=@Angelpro09_Dev
 export BUILD_HOSTNAME=T800-machine
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch aosp_sapphire-ap4a-user
+rm -rf hardware/xiaomi/megvii
 mka bacon
